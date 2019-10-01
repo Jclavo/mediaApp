f56899e3afd7d11298b80d94aef1219321d3a380
jose <jclavotafur@gmail.com>
2019-10-01 16:09:05 -0300

    basic audio controls

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Media, MediaObject } from '@ionic-native/media/ngx';
 
-const file: MediaObject = this.media.create('file.mp3');
+//const file: MediaObject = this.media.create('https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3');
 
 @Component({
   selector: 'app-home',
@@ -11,23 +11,49 @@ const file: MediaObject = this.media.create('file.mp3');
 
 export class HomePage {
 
-  
+  public file: MediaObject;
   
   constructor(private media: Media)
   {
-    
+    this.file = this.media.create('https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3');
+    this.file.onStatusUpdate.subscribe(status => console.log(status));
   }
 
   playAudio()
   {
     // play the file
-    file.play();
+    this.file.play();
   }
 
   stopAudio()
   {
     //
-    file.stop();
+    this.file.stop();
+  }
+
+  pauseAudio()
+  {
+    this.file.pause();
+  }
+
+  getPosition()
+  {
+    // get current playback position
+    this.file.getCurrentPosition().then((position) => {
+      console.log(position);
+    });
+  }
+
+  getDuration()
+  {
+    // get file duration
+    let duration = this.file.getDuration();
+    console.log(duration);
+  }
+
+  realease()
+  {
+    this.file.release();
   }
 
 
--- src/app/readme.txt
+++ src/app/readme.txt
@@ -3,4 +3,5 @@ Things to install
 ionic cordova plugin add cordova-plugin-media
 npm install @ionic-native/media
 
-
+ionic cordova platform add browser
+ionic cordova run browser 
