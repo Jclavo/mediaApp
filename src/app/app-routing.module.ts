20b6a79e3613042219af33fea6c834a16763ef39
Jclavo <jclavotafur@gmail.com>
2019-10-12 18:44:26 -0300

    genre crud

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)},
   { path: 'song', loadChildren: './pages/song/song.module#SongPageModule' },
+  { path: 'genre', loadChildren: './pages/genre/genre.module#GenrePageModule' },
 ];
 
 @NgModule({
